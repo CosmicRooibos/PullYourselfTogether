1a44894cc3785cf1f1dbae73faa8ed33a93f4861
Jacob <sprech41@gmail.com>
2021-01-28 22:40:44 -0500

    Pickups
    Added pickup prefabs and scripts

---
+++ Assets/Scripts/JumpPickup.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPickup : MonoBehaviour
+{
+    //variable for floating animation
+    public float degreesPerSecond = 15.0f;
+    public float amplitude = 0.5f;
+    public float frequency = 1f;
+
+    //position storage for floating animation
+    Vector2 posOffset = new Vector2();
+    Vector2 tempPos = new Vector2();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //store starting position
+        posOffset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float up and down
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+        transform.position = tempPos;
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<MonsterProperties>() != null)
+        {
+            collision.gameObject.GetComponent<MonsterProperties>().JumpAcquired();
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Scripts/MonsterProperties.cs
+++ Assets/Scripts/MonsterProperties.cs
@@ -4,5 +4,49 @@ using UnityEngine;
 
 public class MonsterProperties : MonoBehaviour
 {
+    public bool jumpActive;
+    public bool swimActive;
+    public bool sinkActive;
+    public bool climbActive;
+    public bool squeezeActive;
 
+    void Start()
+    {
+        jumpActive = false;
+        swimActive = false;
+        sinkActive = false;
+        climbActive = false;
+        squeezeActive = false;
+    }
+
+    //Any user feedback will go into these functions
+    public void JumpAcquired()
+    {
+        jumpActive = true;
+        Debug.Log("Jump Power get!!!");
+    }
+
+    public void SwimAcquired()
+    {
+        swimActive = true;
+        Debug.Log("Swim Power get!!!");
+    }
+
+    public void ClimbAcquired()
+    {
+        climbActive = true;
+        Debug.Log("Climb Power get!!!");
+    }
+
+    public void SqueezeAcquired()
+    {
+        squeezeActive = true;
+        Debug.Log("Squeeze Power get!!!");
+    }
+
+    public void SinkAcquired()
+    {
+        sinkActive = true;
+        Debug.Log("Sink Power get!!!");
+    }
 }

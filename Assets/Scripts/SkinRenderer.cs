2b4e229cb33f3d89a29e9408647cda5490c8229e
Jacob <sprech41@gmail.com>
2021-01-30 16:47:44 -0500

    WIP soft body
    Testing soft body for filling chonk

---
+++ Assets/Scripts/SkinRenderer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkinRenderer : MonoBehaviour
+{
+    public Transform twin;
+    // Use this for initialization
+    void Start()
+    {
+        //transform.GetComponent<LineRenderer>().positionCount = parentSkin.childCount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.GetComponent<LineRenderer>().SetPosition(0, transform.position);
+        transform.GetComponent<LineRenderer>().SetPosition(1, twin.position);
+
+        /*
+        for (int i = 0; i < parentSkin.childCount; i++)
+        {
+            transform.GetComponent<LineRenderer>().SetPosition(i, parentSkin.GetChild(i).position);
+        }
+        */
+    }
+}
---
+++ Assets/Scripts/SoftBody.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class SoftBody : MonoBehaviour
+{
+    //private const float splineOffset = 0.5f;
+    [SerializeField]
+    public SpriteShapeController spriteShape;
+    [SerializeField]
+    public Transform[] points;
+
+    void Awake()
+    {
+        /*
+        points = new Transform[transform.childCount - 1];
+        for (int i = 0; i < transform.childCount - 1; i++)
+        {
+            points[i] = transform.GetChild(i);
+        }
+        */
+        UpdateVertices();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateVertices();
+    }
+
+    private void UpdateVertices()
+    {
+        for (int i = 0; i < points.Length; i++)
+        {
+            /*
+            Vector2 vertex = points[i].localPosition;
+            Vector2 towardsCenter = (Vector2.zero - vertex).normalized;
+            float colliderRadius = points[i].gameObject.GetComponent<CircleCollider2D>().radius;
+            
+            try
+            {
+                spriteShape.spline.SetPosition(i, (vertex - towardsCenter * colliderRadius));
+            }
+            catch
+            {
+                Debug.Log("Spline points too close, recalculating");
+                spriteShape.spline.SetPosition(i, (vertex - towardsCenter * (colliderRadius + splineOffset)));
+            }
+            */
+            spriteShape.spline.SetPosition(i, points[i].position);
+        }
+    }
+}

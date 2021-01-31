14c98eb6bbea806f26dba96fd38b0b081afb0005
Anne <jdshaffer93@gmail.com>
2021-01-31 02:27:10 -0500

    Updated CameraFollow behavior
    camera is higher up when chonk is on land (checks the boolean isUnderwater, which will always be false because it is currently not getting any changes from MonsterProperties)

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -11,29 +11,37 @@ Makes the camera follow chonk around as they approach the edge of the screen
 public class CameraFollow : MonoBehaviour
 {
 
-    public GameObject chonk; //put chonk in the space shown in the editor please
+    public GameObject chonkCore; //put chonk in the space shown in the editor please
+
+    bool isUnderwater = false;
+    public int onLandOffset = 5;
+    Vector3 onLandOffsetVector;
 
     void Start()
     {
+        onLandOffsetVector = new Vector3(0, onLandOffset, Camera.main.transform.position.z);
         //get chonk's properties so this script knows when they are on land or in the water so the camera offset can be changed accordingly
     }
 
     void Update()
     {
-        //i also want to separate the camera x and y movements
             MoveCamera();
     }
-
 
     Vector3 pullDirection;
     Vector3 destination;
     public float cameraOffset = 1.5f;
     
     void MoveCamera(){
-        pullDirection = (Camera.main.ScreenToViewportPoint(Input.mousePosition) - Camera.main.WorldToViewportPoint(chonk.transform.position));
-        destination = chonk.transform.position + cameraOffset*pullDirection;
+        pullDirection = (Camera.main.ScreenToViewportPoint(Input.mousePosition) - Camera.main.WorldToViewportPoint(chonkCore.transform.position));
+
+        if(isUnderwater){
+            destination = chonkCore.transform.position + cameraOffset*pullDirection;
+        }
+        else{
+            destination = chonkCore.transform.position + cameraOffset*pullDirection + onLandOffsetVector;
+        }
         destination.z = Camera.main.transform.position.z;
         transform.position = destination;
     }
-
 }
